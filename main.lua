47c420c1e6b689b26b1f2b181c38e1f0815c65a0
brandondsherman <brandondsherman@gmail.com>
2018-05-12 12:15:30 -0700

    parallax

--- main.lua
+++ main.lua
@@ -6,7 +6,14 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 local background = love.graphics.newImage('data/background.png')
-local ground = love.graphics.newImage('data/ground.png')
+local backgroundScroll = 0
+local BACKGROUND_SCROLL_SPEED = 30
+local BACKGROUND_LOOPING_POINT = 413
+
+local ground = love.graphics.newImage('data/ground.png') 
+local groundScroll = 0
+local GROUND_SCROLL_SPEED = 60
+--local GROUND_LOOPING_POINT = VIRTUAL_WIDTH
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -32,10 +39,18 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+        % BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+        % VIRTUAL_WIDTH
+end
+
 function love.draw()
     push:start()
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(background, -1 * backgroundScroll, 0)
+    love.graphics.draw(ground, -1 * groundScroll, VIRTUAL_HEIGHT - 16)
 
     push:finish()
 end
